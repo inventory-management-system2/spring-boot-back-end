d86a4f852286aa21dccee27c60a75b93d13c941d
marceloarb <59586505+marceloarb@users.noreply.github.com>
2020-07-30 16:05:15 -0500

    Initial commit

---
+++ src/main/java/com/teksystems/Capstone3BackEnd/Capstone3BackEndApplication.java
@@ -0,0 +1,13 @@
+package com.teksystems.Capstone3BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Capstone3BackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Capstone3BackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teksystems/Capstone3BackEnd/Capstone3BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teksystems.Capstone3BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Capstone3BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
