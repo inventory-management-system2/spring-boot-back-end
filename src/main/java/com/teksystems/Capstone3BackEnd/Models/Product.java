878740752f438237f3b86197b6fdd49eb55b43dd
John Bozarov <codinginusa@gmail.com>
2020-07-31 09:47:14 -0400

    test commit

---
+++ src/main/java/com/teksystems/Capstone3BackEnd/Models/Product.java
@@ -0,0 +1,11 @@
+package com.teksystems.Capstone3BackEnd.Models;
+
+public class Product {
+
+	public static void main(String[] args) {
+		
+		System.out.println("John added this test class");
+
+	}
+
+}
