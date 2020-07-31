f1c0e193cb4c1c26040e63254ac5bde92e563197
Korey Brooks <korey.brooks@mail.com>
2020-07-31 16:14:24 -0500

    product controller test

---
+++ src/test/java/com/teksystems/Capstone3BackEnd/Controller/ProductControllerTest.java
@@ -0,0 +1,33 @@
+package com.teksystems.Capstone3BackEnd.Controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.teksystems.Capstone3BackEnd.controller.ProductController;
+
+
+@SpringBootTest
+public class ProductControllerTest {
+	
+		
+		@Autowired
+		private ProductController controller;
+		
+		@Test
+		public void contextLoads() throws Exception {
+			fail();
+			
+		}
+		
+	}
+
+	
+		
+	
+	
+	
+
